0d6ab5f6b641912dd81a541addbbd60f349ddf80
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-03-07 22:33:28 +0700

    .

---
+++ view/announce/add_announce.php
@@ -0,0 +1,21 @@
+<style>
+    .content {
+        width: 100%;
+        background-color: #FFCFCF;
+        border-radius: 20px;
+        height: 500px;
+        padding: 20px;
+    }
+</style>
+<?php require_once("menuTop.php") ?>
+<div>
+    <h3>เพิ่มประกาศ</h3>
+    <hr style="width:100%;text-align:left;margin-left:0">
+    <div class="content">
+    <form>
+    <span style="font-size:25px; ">ชื่อประกาศ</span>
+    <input placeholder="กรอกชื่อประกาศ" type="text" class="fs-5 h-10 text-start w-20" style="margin-left: 37px;"/>
+    </form>
+    </div>
+</div>
+<?php require_once("menuDown.php") ?>
