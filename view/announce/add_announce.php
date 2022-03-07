03ef0af3b403de188027ad283357f5ec61e824a3
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-03-07 23:00:42 +0700

    Update add_announce.php

--- view/announce/add_announce.php
+++ view/announce/add_announce.php
@@ -3,7 +3,7 @@
         width: 100%;
         background-color: #FFCFCF;
         border-radius: 20px;
-        height: 500px;
+        height: 700px;
         padding: 20px;
     }
 </style>
@@ -12,10 +12,19 @@
     <h3>เพิ่มประกาศ</h3>
     <hr style="width:100%;text-align:left;margin-left:0">
     <div class="content">
-    <form>
-    <span style="font-size:25px; ">ชื่อประกาศ</span>
-    <input placeholder="กรอกชื่อประกาศ" type="text" class="fs-5 h-10 text-start w-20" style="margin-left: 37px;"/>
-    </form>
+        <form>
+            <br>
+            <span style="font-size:25px; margin-left: 60px;">ชื่อประกาศ:</span>
+            <input placeholder="กรอกชื่อประกาศ" type="text" style="margin-left: 95px;" /><br><br>
+            <span style="font-size:25px; margin-left: 60px;">เนื้อหาประกาศ:</span><br>
+            <div class="mb-3">
+                <textarea placeholder="กรอกเนื้อหาประกาศ" name="Condition" type="text" rows="3" style="width: 1000px; height: 238px; margin-left: 275px;"></textarea>
+            </div><br><br>
+            <span style="font-size:25px; margin-left: 60px;">แนบเอกสาร:</span>
+            <a type="button" class="btn" style="margin-left: 80px; background-color: black; border-radius: 35px; color: #FFFFFF;" >เลือกไฟล์</a><br><br>
+            <a type="button" class="btn" style="margin-left: 700px; background-color: black; border-radius: 35px; color: #FFFFFF;" >อัพโหลด</a>
+
+        </form>
     </div>
 </div>
 <?php require_once("menuDown.php") ?>
