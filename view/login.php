4a2e15fd400af580ce1e79d8df242ac178425a81
Fourth1755 <fourthe.hero@gmail.com>
2022-03-08 21:23:01 +0700

    login

--- view/login.php
+++ view/login.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<body>
-<html>
-<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 <style>
-img {
-  border-radius: 5px;
-  float: left;
-  padding-left: 250px;
-  justify-content: flex-end;
-  align-items: flex-end;
+body{
+  margin: 0;
+  font-family:'Prompt', sans-serif;
 }
-</style>
-</head>
-<body>
-    <h2>ระบบจัดการการฝึกงาน</h2>
-    <img src="https://upload.wikimedia.org/wikipedia/th/a/a4/%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B8%A7%E0%B8%B4%E0%B8%A8%E0%B8%A7%E0%B8%81%E0%B8%A3%E0%B8%A3%E0%B8%A1%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C_%E0%B8%81%E0%B8%B3%E0%B9%81%E0%B8%9E%E0%B8%87%E0%B9%81%E0%B8%AA%E0%B8%99_%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%95%E0%B8%A3%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C.png" alt="Cinque Terre" width="400" height="300">
-  </a>
-</div>
-<html>
-<style>
+.logo-img {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+.logo-img img{
+  width: 280px;
+  height: 260px;
+}
 .container {
   padding: 16px;
 }
@@ -31,21 +30,6 @@ span.psw {
   float: right;
   padding-top: 16px;
 }
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #171738;
-  border-radius: 15px;
-
-}
-button {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #171738;
-  color: white;
-  border-radius: 15px;
-}
-
 button:hover {
   opacity: 0.8;
 }
@@ -66,7 +50,6 @@ input[type=text], select {
   border: 1px solid #ccc;
   border-radius: 5px;
   box-sizing: border-box;
-  background-color: #FFCFCF;
 }
 
 input[type=submit], select {
@@ -77,37 +60,86 @@ input[type=submit], select {
   border: none;
   border-radius: 20px;
   cursor: pointer;
-  
 }
-
-div {
-  border-radius: 5px;
-  padding: 20px;
-  background-color: white;
+.form-login{
+  width: 500px;
+  padding: 40px;
+  background-color: #FFCFCF;
+  margin: 30px;
+  border-radius: 20px;
+}
+.content-login{
+  display: flex;
+  justify-content: space-between;
+}
+#button-black{
+  color : #FFF;
+  background-color: #171738;
+  border-radius: 15px;
+  padding: 12px 15px 12px 15px;
+}
+#button-black:hover{
+  color : #FFCFCF;
+  background-color: #000;
+  border-radius: 15px;
+}
+.form-bigimage{
+  border-right: 400px solid transparent;
+  height: 0;
+  border-bottom: 800px solid #171738;
+  width: 70%;
+  color: #FFF;
+  z-index: -1;
+}
+.logo-bigimg{
+  z-index: 1;
+}
+.logo-bigimg img{
+  height: 600px;
+  width: 600px;
 }
 </style>
-
+</head>
 <body>
-
-<div>
-  
-<div class="container">
-  <div><div><div><div><div><div><div></div></div></div></div></div></div></div>
-    <label for="uname"><b>บัญชีผู้ใช้เครือข่ายนนทรี</b></label>
+  <div class="content-login">
+    <div class="form-bigimage">
+    <h2>ระบบจัดการการฝึกงาน</h2>
+    <div class="logo-bigimg">
+    <img src="https://www.pngmart.com/files/5/Gears-PNG-Pic.png" alt="Cinque Terre">
+    </div>
+    </div>
+    <div class="form-login">  
+    <div class="logo-img">
+    <img src="https://upload.wikimedia.org/wikipedia/th/a/a4/%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B8%A7%E0%B8%B4%E0%B8%A8%E0%B8%A7%E0%B8%81%E0%B8%A3%E0%B8%A3%E0%B8%A1%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C_%E0%B8%81%E0%B8%B3%E0%B9%81%E0%B8%9E%E0%B8%87%E0%B9%81%E0%B8%AA%E0%B8%99_%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%95%E0%B8%A3%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C.png" alt="Cinque Terre" width="400" height="300">
+    </div>
+    <form>
+      <label for="uname"><b>บัญชีผู้ใช้เครือข่ายนนทรี</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
-
     <label for="psw"><b>รหัสผ่าน</b></label>
     <input type="text" placeholder="Enter Password" name="psw" required>
-        
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me <span class="psw">ลืมรหัสผ่าน<a href="?controller=singup&action=index"> password?</a></span>
-    </label>
+    
+    <div style="padding: 20px;display: flex;justify-content: space-between;">
+      <div><input type="checkbox" checked="checked" name="remember"> Remember me</div>
+       <div>ลืมรหัสผ่าน<a href="?controller=singup&action=index" style="text-decoration: none;"> password?</a></div>
+    </div>
+    <div style="padding: 5px;display: flex;justify-content: space-around;">
+      <div><a class="btn" href="?controller=announce&action=index" id="button-black">กลับหน้าหลัก</a></div>
+      <div><button class="btn" type="submit" href="?controller=announce&action=index" id="button-black">เข้าสู่ระบบ</button></div>
+    </div>
+    </form>
   </div>
-
-  <div class="container">
-  <button type="submit" href="?controller=announce&action=index">เข้าสู่ระบบ</button>
-  <a type="submit" class="cancelbtn" href="?controller=announce&action=index" style="color : #FFF;">กลับหน้าหลัก</a>
-</form>
-</div>
+  </div>
+ 
+  
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
