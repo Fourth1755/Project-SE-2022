53d2aadc31f6a2ed329890f29579f81c6f93cafe
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-03-09 20:41:49 +0700

    อัพโหลดรายงาน

--- view/uploadreport/index_uploadreport.php
+++ view/uploadreport/index_uploadreport.php
@@ -1,8 +1,75 @@
+<style>
+      .content {
+            width: 100%;
+            background-color: #FFCFCF;
+            border-radius: 2px;
+            height: 300;
+            padding: 20px;
+      }
+</style>
 <?php require_once("menuTop.php") ?>
 <div>
       <h3>อัพโหลดรายงาน</h3>
-      
       <hr style="width:100%;text-align:left;margin-left:0">
-      
+      <div class="content">
+            <br>
+            <div style="display: flex; margin-left: 25px;">
+                  <label style="font-size:20px;">สถานะการส่ง</label>
+                  <label style="font-size:20px; margin-left: 150px;">Submitted</label>
+            </div>
+            <hr style="width:100%;text-align:left;margin-left:0">
+            <div style="display: flex; margin-left: 25px;">
+                  <label style="font-size:20px;">แก้ไขล่าสุด</label>
+                  <label style="font-size:20px; margin-left: 170;">Monday, 7 February 2023, 11:55PM</label>
+            </div>
+            <hr style="width:100%;text-align:left;margin-left:0">
+            <div style="display: flex; margin-left: 25px;">
+                  <label style="font-size:20px;">File submissions</label>
+                  <div>
+                        <label style="font-size:20px; margin-left: 110px;"><i class="fas fa-file-pdf"></i> รายงาน_622*******</label>
+                        <br>
+                        <label style="font-size:20px; margin-left: 110px;"><i class="fas fa-file-word"></i> นำเสนอการฝึึกงาน_622*******</label>
+                  </div>
+            </div>
+
+
+      </div>
+      <br>
+      <a type="button" class="btn" style="margin-left: 700px; background-color: #171738; border-radius: 10px; color: #FFFFFF;  font-size:20px;">Edit submissions </a>
+      <br><br>
+      <div class="content">
+            <br>
+            <div style=" margin-left: 25px;">
+                  <label style="font-size:20px;">File submissions</label>
+                  <br><br>
+                  <div style="display: flex;">
+                        <div style="margin-left: 25px; ">
+                              <img src="https://uxwing.com/wp-content/themes/uxwing/download/04-file-folder-type/pdf-files.png" style="width: 60px;"><br>
+                              <label>รายงาน_622*******</label>
+                        </div>
+                        <div style="margin-left: 50px; ">
+                              <img src="https://www.pngrepo.com/png/352755/512/file-word.png" style="width: 60px;"><br>
+                              <label>นำเสนอการฝึึกงาน_622*******</label>
+                        </div>
+                        <div style="margin-left: 50px; ">
+                              <img src="https://uxwing.com/wp-content/themes/uxwing/download/04-file-folder-type/pdf-files.png" style="width: 60px;"><br>
+                              <label>ประเมิน_622*******</label>
+                        </div>
+
+                  </div>
+            </div>
+
+
+      </div>
+
+      <br>
+
+     
+      <div style="display: flex; margin-left: 650px;" >
+            <a type="button" class="btn" style=" background-color: #EB2626; border-radius: 10px; color: #FFFFFF;  font-size:20px;">ยกเลิก </a>
+            <a type="button" class="btn" style="margin-left: 40px; background-color: #171738; border-radius: 10px; color: #FFFFFF;  font-size:20px;">บันทึกการเปลี่ยนแปลง </a>
+      </div>
+
+
 </div>
 <?php require_once("menuDown.php") ?>
