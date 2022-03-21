672762e94a7a000617ff6906db65ab387e1f240b
NuttawatJuntawong <91516482+NuttawatJuntawong@users.noreply.github.com>
2022-03-21 19:52:34 +0700

    เพิ่มเติม

--- view/petition/newcompany_petition.php
+++ view/petition/newcompany_petition.php
@@ -17,69 +17,69 @@
             <form method="get" action="">
                   <div class="form-group pb-4 col-md-4">
                   <label>ชื่อสถานประกอบการ</label>
-                  <input type="tel" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="กรอกชื่อสถานประกอบการ">
+                  <input type="tel" class="form-control" name="name" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="กรอกชื่อสถานประกอบการ" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ประเภทธุรกิจ</label>
-                  <input type="text" class="form-control" name="type" id="exampleInputPassword1" placeholder="กรอกประเภทธุรกิจ">
+                  <input type="text" class="form-control" name="type" id="exampleInputPassword1" placeholder="กรอกประเภทธุรกิจ" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ที่อยู่ของสถานประกอบการ</label>
-                  <input type="text" class="form-control" name="address" id="exampleInputPassword1" placeholder="กรอกที่อยู่">
+                  <input type="text" class="form-control" name="address" id="exampleInputPassword1" placeholder="กรอกที่อยู่" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>เบอร์โทรศัพท์ของสถานประกอบการ</label>
-                  <input type="text" class="form-control" name="phoneNumber" id="exampleInputPassword1" placeholder="กรอกเบอร์โทร">
+                  <input type="text" class="form-control" name="phoneNumber" id="exampleInputPassword1" pattern="[0-9]{10}" placeholder="กรอกเบอร์โทร" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>เบอร์โทรสารของสถานประกอบการ</label>
-                  <input type="text" class="form-control" name="faxNumber" id="exampleInputPassword1" placeholder="กรอกเบอร์โทรสาร">
+                  <input type="text" class="form-control" name="faxNumber" id="exampleInputPassword1" pattern="[0-9]{10}" placeholder="กรอกเบอร์โทรสาร" required>
                   </div>
                   <hr style="width:100%;text-align:left;margin-left:0;">
                   <h4>ข้อมูลผู้ติดต่อของสถานประกอบการ</h4>
                   <div class="form-group pb-4 col-md-4">
                   <label>ชื่อหัวหน้างาน/ผู้อนุมัติการฝึกงาน</label>
-                  <input type="text" class="form-control" name="agentName" id="exampleInputPassword1" placeholder="กรอกชื่อ">
+                  <input type="text" class="form-control" name="agentName" id="exampleInputPassword1" placeholder="กรอกชื่อ" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ตำแหน่งของหัวหน้างาน/ผู้อนุมัติการฝึกงาน</label>
-                  <input type="text" class="form-control" name="agentPosition" id="exampleInputPassword1" placeholder="กรอกตำแหน่ง">
+                  <input type="text" class="form-control" name="agentPosition" id="exampleInputPassword1" placeholder="กรอกตำแหน่ง" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ชื่อผู้ประสานงานของสถานประกอบการ/หน่วยงาน</label>
-                  <input type="text" class="form-control" name="HR_Name" id="exampleInputPassword1" placeholder="กรอกชื่อ">
+                  <input type="text" class="form-control" name="HR_Name" id="exampleInputPassword1" placeholder="กรอกชื่อ" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ตำแหน่งของผู้ประสานงาน</label>
-                  <input type="text" class="form-control" name="HR_Position" id="exampleInputPassword1" placeholder="กรอกตำแหน่ง">
+                  <input type="text" class="form-control" name="HR_Position" id="exampleInputPassword1" placeholder="กรอกตำแหน่ง" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>เบอร์โทรศัพท์ของผู้ประสานงาน</label>
-                  <input type="text" class="form-control" name="HR_PhoneNamber" id="exampleInputPassword1" placeholder="กรอกเบอร์โทร">
+                  <input type="text" class="form-control" name="HR_PhoneNamber" id="exampleInputPassword1" pattern="[0-9]{10}" placeholder="กรอกเบอร์โทร" required>
                   </div>
                   <hr style="width:100%;text-align:left;margin-left:0;">
                   <h4>ตำแหน่งงานที่ต้องการรับนิสิตฝึกงาน</h4>
                   <div class="form-group pb-4 col-md-4">
                   <label>ตำแหน่งงานที่ต้องการรับ</label>
-                  <input type="text" class="form-control" name="requestPosition" id="exampleInputPassword1" placeholder="กรอกตำแหน่งงาน">
+                  <input type="text" class="form-control" name="requestPosition" id="exampleInputPassword1" placeholder="กรอกตำแหน่งงาน" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ลักษณะงานที่นิสิตต้องปฏิบัติ (Job Description)</label>
-                  <input type="text" class="form-control" name="jobDescription" id="exampleInputPassword1" placeholder="กรอกลักษณะงาน">
+                  <input type="text" class="form-control" name="jobDescription" id="exampleInputPassword1" placeholder="กรอกลักษณะงาน" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>จำนวน (คน)</label>
-                  <input type="text" class="form-control" name="amountPerson" id="exampleInputPassword1" placeholder="กรอกจำนวนคน">
+                  <input type="text" class="form-control" name="amountPerson" id="exampleInputPassword1" pattern="[1-9][0-9]" placeholder="กรอกจำนวนคน" required>
                   </div>
                   <div class="form-group pb-4 col-md-4">
                   <label>ความสามารถทางวิชาการหรือทักษะที่นิสิตควรมี</label>
-                  <input type="text" class="form-control" name="requiredSkill" id="exampleInputPassword1" placeholder="กรอกทักษะ/ความสามารถที่ควรมี">
+                  <input type="text" class="form-control" name="requiredSkill" id="exampleInputPassword1" placeholder="กรอกทักษะ/ความสามารถที่ควรมี" required>
                   </div>
                   <label>ระยะเวลาที่ต้องการให้นิสิตไปปฏิบัติงาน</label>
                   <div class="form-group pb-4 col-md-4" style="display: flex;">
-                        <input type="date" name="startDate" class="form-control">
+                        <input type="date" name="startDate" class="form-control" required min="<?php echo date('Y-m-d');?>">
                         <label style="padding: 8px;">ถึง</label>
-                        <input type="date" name="endDate" class="form-control">
+                        <input type="date" name="endDate" class="form-control" required min="<?php echo date('Y-m-d');?>">
                   </div>
                   <input name="controller" value="company" type="hidden"/>
                   <br>
